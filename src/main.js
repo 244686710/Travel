5845a4c9abbac4a9cc09cffdf9cb044ad03b7bc0
yu.yadong <yuyadong@zgrj.net>
2018-10-18 14:55:47 +0800

    '引入fastclick'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import fastClick from 'fastclick';
 import App from './App';
 import router from './router';
+import './assets/styles/reset.css';
 
 Vue.config.productionTip = false;
+fastClick.attach(document.body); // 移动端click解决
 
 /* eslint-disable no-new */
 new Vue({
