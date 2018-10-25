3dc82e804a2003a6f20e7670246f383bf0d6b742
yu.yadong <yuyadong@zgrj.net>
2018-10-25 17:23:33 +0800

    'index-swiper'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import FastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
 import 'styles/reset.css'
 import 'styles/iconfont.css'
 
+// require styles
+import 'swiper/dist/css/swiper.css'
+
 Vue.config.productionTip = false
 FastClick.attach(document.body) // vue移动端使用click
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="swiper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="(item, index) in swiperList" :key="index">
+        <img class="swiper-img" :src="item.imgURL" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperList: [
+        {imgURL: 'http://image.btc1999.com/img/logo/6b3e7f9b-f923-4c8c-8066-62e3a6b7342e.jpg'},
+        {imgURL: 'http://image.btc1999.com/img/logo/fcf67ce5-3eda-45ac-8253-20581397574d.jpg'}
+      ],
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .swiper >>> .swiper-pagination-bullet-active
+    background #fff
+  .swiper
+    box-sizing border-box
+    width 100%
+    height 0
+    padding-bottom 31.25%
+    background #eee
+    .swiper-img
+      width 100%
+</style>
